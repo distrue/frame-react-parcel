3f199d28ecde0debeb741423652de8c80121244f
Y.Jung <gilsang9808@gmail.com>
2019-12-31 23:28:29 +0900

    typescript, eslint 전환

--- parcel.config.js
+++ parcel.config.js
@@ -1,8 +1,8 @@
-const Bundler = require('parcel-bundler')
-const Path = require('path')
+const Bundler = require('parcel-bundler');
+const Path = require('path');
 
 // 진입점 파일 위치
-const file = Path.join(__dirname, '../src/index.html')
+const file = Path.join(__dirname, '../src/index.html');
 
 // 번들러 옵션
 const options = {
@@ -19,16 +19,16 @@ const options = {
   hmrPort: 0, // HMR 소켓이 돌아갈 포트번호. 기본값은 무작위의 빈 포트 (node.js에서 0은 무작위의 빈 포트로 배정됨)
   sourceMaps: true, // 소스맵을 활성화할지 여부. 기본값은 활성화 (아직 미니파이드 빌드에선 지원되지 않음)
   hmrHostname: '', // 빠른 모듈 교체를 위한 hostname.기본값은 ''
-  detailedReport: false // 번들, 애셋, 파일 크기, 빌드 시간을 담은 상세한 리포트를 출력. 기본값은 false. 리포트는 오직 watch가 비활성일때만 출력됨
-}
+  detailedReport: false, // 번들, 애셋, 파일 크기, 빌드 시간을 담은 상세한 리포트를 출력. 기본값은 false. 리포트는 오직 watch가 비활성일때만 출력됨
+};
 
 async function runBundle() {
   // 진입점 위치와 옵션을 제공해서 번들러를 초기화
-  const bundler = new Bundler(file, options)
+  const bundler = new Bundler(file, options);
 
   // 번들러를 실행함. 이것은 메인 번들을 반환함.
   // watch 모드를 사용할 경우 이 promise는 매 빌드마다 호출하는게 아니라 딱 한 번만 호출하고 이벤트를 사용할 것
-  const bundle = await bundler.bundle()
+  await bundler.bundle();
 }
 
-runBundle()
+runBundle();
